95eee9253410323e91bcd947e2db0c7be6b390ea
Mateusz Dabrowicz <dabrowicz.mateusz@gmail.com>
2021-10-20 15:04:52 +0200

    pierwszy test

---
+++ src/test/java/PierwszyTest.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PierwszyTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void pierwszyTest(){
+        driver.get("https://google.pl");
+        driver.findElement(By.id("L2AGLb")).click();
+        assertTrue( driver.findElement(By.name("q")).isDisplayed());
+    }
+}
